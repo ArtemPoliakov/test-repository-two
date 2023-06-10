f6e85530ec1aed01fb9fd08e5434813621faafa9
ArtemPoliakov <apoliakov50@gmail.com>
2023-06-10 16:56:22 +0300

    Adding a new method HowDoYouDo

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hi GIT");
     }
+
+    public static void sayHowDoYouDo(){
+        System.out.println("How do you do?????????????????");
+    }
 }
